1a9617e96975e88d556f3c9455bfa80a19065aca
[jérémy morlier] <[jeremy.morlier@imt-atlantique.fr]>
2024-06-03 11:32:55 +0200

    fix

--- test_rank.py
+++ test_rank.py
@@ -3,14 +3,6 @@ import torchvision_references.references.segmentation.utils as utils
 
 
 def main(args):
-    if args.backend.lower() != "pil" and not args.use_v2:
-        # TODO: Support tensor backend in V1?
-        raise ValueError("Use --use-v2 if you want to use the tv_tensor or tensor backend.")
-    if args.use_v2 and args.dataset != "coco":
-        raise ValueError("v2 is only support supported for coco dataset for now.")
-
-    if args.output_dir:
-        utils.mkdir(args.output_dir)
 
     utils.init_distributed_mode(args)
     print(args)
