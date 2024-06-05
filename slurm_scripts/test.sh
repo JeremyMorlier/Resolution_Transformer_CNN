c6da336ce016cdd5d18a804a20aaed02211281a4
[jérémy morlier] <[jeremy.morlier@imt-atlantique.fr]>
2024-06-05 16:26:29 +0200

    test

--- slurm_scripts/test.sh
+++ slurm_scripts/test.sh
@@ -7,8 +7,8 @@
 #SBATCH --ntasks=4 # reserver 4 taches (ou processus)
 #SBATCH --gres=gpu:4 # reserver 4 GPU
 #SBATCH --cpus-per-task=10 # reserver 10 CPU par tache (et memoire associee)
-#SBATCH --time=20:00:00 # temps maximal d’allocation "(HH:MM:SS)"
-#SBATCH --qos=qos_gpu-t3 # QoS
+#SBATCH --time=00:5:00 # temps maximal d’allocation "(HH:MM:SS)"
+#SBATCH --qos=qos_gpu-dev # QoS
 #SBATCH --hint=nomultithread # desactiver l’hyperthreading
 #SBATCH --account=sxq@v100 # comptabilite V100
 
