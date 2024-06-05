f3ea020fc29ae2ce02c7fa5498d18ffa0364e9d1
[jérémy morlier] <[jeremy.morlier@imt-atlantique.fr]>
2024-06-05 14:35:02 +0200

    fix test_rank script

--- test_rank.py
+++ test_rank.py
@@ -1,10 +1,13 @@
 
-import torchvision_references.references.segmentation.utils as utils
+import torchvision_references.references.segmentation.utils as utils1
+import torchvision_references.references.segmentation.utils as utils2
 
+def main(args, parser1):
 
-def main(args):
-
-    utils.init_distributed_mode(args)
+    if parser1:
+        utils1.init_distributed_mode(args)
+    else :
+        utils2.init_distributed_mode(args)
     print(args)
 
 def get_args_parser(add_help=True):
@@ -13,9 +16,12 @@ def get_args_parser(add_help=True):
     parser = argparse.ArgumentParser(description="PyTorch Segmentation Training", add_help=add_help)
 
     parser.add_argument("--patate", default="2", type=str, help="dataset path")
+    parser.add_argument("--world-size", default=1, type=int, help="number of distributed processes")
+    parser.add_argument("--dist-url", default="env://", type=str, help="url used to set up distributed training")
 
     return parser
 
 if __name__ == "__main__":
     args = get_args_parser().parse_args()
-    main(args)
+    print(args)
+    main(args, True)
