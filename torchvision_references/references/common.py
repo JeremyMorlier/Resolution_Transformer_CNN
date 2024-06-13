c220a1f5f832a64b9066b7ee9d2f214ae1e46b75
JeremyMorlier <76213539+JeremyMorlier@users.noreply.github.com>
2024-06-13 10:44:05 +0200

    modified permission update

--- torchvision_references/references/common.py
+++ torchvision_references/references/common.py
@@ -4,4 +4,4 @@ import stat
 def create_dir(dir) :
     if not os.path.isdir(dir) :
         os.mkdir(dir)
-    os.chmod(dir, stat.S_IRWXU | stat.S_IRWXO)
+        os.chmod(dir, stat.S_IRWXU | stat.S_IRWXO)
