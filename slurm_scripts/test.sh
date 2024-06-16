6ef3cfff348f03146b7a63f0bb7689f56c3fd464
[jérémy morlier] <[jeremy.morlier@imt-atlantique.fr]>
2024-06-16 22:31:23 +0200

    test

--- slurm_scripts/test.sh
+++ slurm_scripts/test.sh
@@ -21,4 +21,5 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 export WANDB_DIR=$WORK/wandb/
 export WANDB_MODE=offline
 
-srun torchrun --standalone --nnodes=1 --nproc-per-node=1 test_rank.py --patate 1 
+srun python -u test_rank.py --patate 1 
+#srun torchrun --standalone --nnodes=1 --nproc-per-node=1 test_rank.py --patate 1 
