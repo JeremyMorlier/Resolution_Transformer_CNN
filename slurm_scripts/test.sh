16c5eb57d1c48b308bdda7928c39f9404bd3d59c
[jérémy morlier] <[jeremy.morlier@imt-atlantique.fr]>
2024-06-17 15:57:44 +0200

    test

--- slurm_scripts/test.sh
+++ slurm_scripts/test.sh
@@ -21,5 +21,15 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 export WANDB_DIR=$WORK/wandb/
 export WANDB_MODE=offline
 
+export MASTER_PORT=12340
+export WORLD_SIZE=4
+
+### get the first node name as master address - customized for vgg slurm
+### e.g. master(gnodee[2-5],gnoded1) == gnodee2
+echo "NODELIST="${SLURM_NODELIST}
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+echo "MASTER_ADDR="$MASTER_ADDR
+
 srun python -u test_rank.py --patate 1 
 #srun torchrun --standalone --nnodes=1 --nproc-per-node=1 test_rank.py --patate 1 
