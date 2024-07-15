15445957724c22553117221bdd075694928b299f
[jérémy morlier] <[jeremy.morlier@imt-atlantique.fr]>
2024-07-15 14:26:51 +0200

    v100 to a100

--- slurm_scripts/resolution/train_vit.sh
+++ slurm_scripts/resolution/train_vit.sh
@@ -2,15 +2,15 @@
 #SBATCH --job-name=VITB # nom du job
 #SBATCH --output=log/VITB/%j/logs.out # fichier de sortie (%j = job ID)
 #SBATCH --error=log/VITB/%j/errors.err # fichier d’erreur (%j = job ID)
-#SBATCH --constraint=v100-32g # demander des GPU a 16 Go de RAM
+#SBATCH --constraint=a100 
 #SBATCH --nodes=1 # reserver 1 nœud
-#SBATCH --ntasks=4 # reserver 4 taches (ou processus)
+#SBATCH --ntasks=4 reserver 4 taches (ou processus)
 #SBATCH --gres=gpu:4 # reserver 4 GPU
-#SBATCH --cpus-per-task=10 # reserver 10 CPU par tache (et memoire associee)
-#SBATCH --time=100:00:00 # temps maximal d’allocation "(HH:MM:SS)"
-#SBATCH --qos=qos_gpu-t4 # QoS
+#SBATCH --cpus-per-task=8 # reserver 10 CPU par tache (et memoire associee)
+#SBATCH --time=20:00:00 # temps maximal d’allocation "(HH:MM:SS)"
+#SBATCH --qos=qos_gpu-t3 # QoS
 #SBATCH --hint=nomultithread # desactiver l’hyperthreading
-#SBATCH --account=sxq@v100 # comptabilite V100
+#SBATCH --account=sxq@a100 # comptabilite V100
 
 module purge # nettoyer les modules herites par defaut
 conda deactivate # desactiver les environnements herites par defaut
