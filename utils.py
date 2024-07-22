fb7ad728e0d90f01d0af02a4508507a06e17cef5
[jérémy morlier] <[jeremy.morlier@imt-atlantique.fr]>
2024-07-22 14:20:36 +0200

    added utils py to read pth files

---
+++ utils.py
@@ -0,0 +1,17 @@
+import argpars
+import torch
+
+
+def args_parser(add_help=True):
+    parser = argparse.ArgumentParser(description="Test", add_help=add_help)
+    parser.add_argument("--path", default="", type=str, help="Path of checkpoint file")
+
+    return parser
+
+def pth_reader(path) :
+    torch_dict = torch.load(path)
+    print(torch_dict.keys())
+
+if __name__ == "__main" :
+    args = args_parser.parse_args()
+    pth_reader(args.path)
