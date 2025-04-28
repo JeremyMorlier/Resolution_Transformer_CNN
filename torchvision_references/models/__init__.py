562f9c39a24bd0cb564fa10eee43924af037c531
Jérémy Morlier <jeremy.morlier@imt-atlantique.fr>
2025-04-28 14:27:51 +0200

    Cleanup unnecessary files

--- torchvision_references/models/__init__.py
+++ torchvision_references/models/__init__.py
@@ -1,22 +1,8 @@
-from .alexnet import *
-from .convnext import *
-from .densenet import *
-from .efficientnet import *
-from .googlenet import *
-from .inception import *
-from .mnasnet import *
-from .mobilenet import *
-from .regnet import *
 from .resnet import *
-from .shufflenetv2 import *
-from .squeezenet import *
-from .vgg import *
 from .vision_transformer import *
-from .swin_transformer import *
-from .maxvit import *
-from . import detection, optical_flow, quantization, segmentation, video
+from . import segmentation
 
-from .SAM import *
+#from .SAM import *
 # The Weights and WeightsEnum are developer-facing utils that we make public for
 # downstream libs like torchgeo https://github.com/pytorch/vision/issues/7094
 # TODO: we could / should document them publicly, but it's not clear where, as
