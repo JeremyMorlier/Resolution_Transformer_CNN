39c2723b6e06045fa90befdb203d9d2870b0c337
Jérémy Morlier <jeremy.morlier@imt-atlantique.fr>
2025-04-28 14:30:12 +0200

    fix

--- plot_cs_vs_res.py
+++ plot_cs_vs_res.py
@@ -8,7 +8,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-import tikzplotlib as tkplot
+# import tikzplotlib as tkplot
 
 import torch
 import torch.utils.data
@@ -25,7 +25,7 @@ import torchvision_references.references.classification.utils as utils
 from torchvision_references.references.classification.sampler import RASampler
 from torchvision_references.models import get_model
 
-import wandb
+#import wandb
 
 from torchinfo import summary
 def get_param_model(first_conv_resize, channels, num_classes) :
@@ -33,6 +33,7 @@ def get_param_model(first_conv_resize, channels, num_classes) :
     
     return model
 
+target = 2087375720
 
 if __name__ == "__main__" :
 
@@ -58,33 +59,62 @@ if __name__ == "__main__" :
     macs = []
     memories = []
     for train_crop in train_crop_size :
+        print(train_crop)
         input_size_train = (1, 3, train_crop, train_crop)
-        info = summary(model, input_size_train, col_names=("output_size", "num_params", "mult_adds"))
+        info = summary(model, input_size_train,verbose=0, col_names=("output_size", "num_params", "mult_adds"))
 
         macs.append(info.total_mult_adds)
-        memories.append(info.max_memory)
-        print(input_size_train, info.total_mult_adds, info.max_memory)
+        memories.append(0)
+        #memories.append(info.max_memory)
+        # print(input_size_train, info.total_mult_adds, info.max_memory)
 
 
     # get inference flops and memory for Resnet50 with different depth scaling
-    eval_channels = [16, 32, 64, 128, 256, 512]
+    # eval_channels = [16, 32, 64, 128, 256, 512]
+    # macs2 = []
+    # memories2 = []
+    # train_crop_size2 = []
+    # channels_list = []
+    # for channels in itertools.product(eval_channels, eval_channels, eval_channels, eval_channels) :
+    #     model = get_param_model(first_conv_resize, channels, num_classes)
+    #     input_size_train = (1, 3, 232, 232)
+    #     info = summary(model, input_size_train, verbose=0, col_names=("output_size", "num_params", "mult_adds"))
+
+    #     channels_list.append(channels)
+    #     macs2.append(info.total_mult_adds)
+    #     memories2.append(info.max_memory)
+    #     train_crop_size2.append(232)
+    #     print(channels, info.total_mult_adds, info.max_memory)
+    
+    channels = [64, 128, 256, 512]
     macs2 = []
     memories2 = []
     train_crop_size2 = []
     channels_list = []
-    for channels in itertools.product(eval_channels, eval_channels, eval_channels, eval_channels) :
-        model = get_param_model(first_conv_resize, channels, num_classes)
-        input_size_train = (1, 3, 232, 232)
+    ratios = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.25, 0.5, 0.75]
+    ratios = np.linspace(0.1, 1, 90)
+
+
+    best = 0
+    best_channels = None
+    for ratio in  ratios :
+        ratio_channels = [int(ratio*element) for element in channels]
+        print(ratio_channels)
+        model = get_param_model(first_conv_resize, ratio_channels, num_classes)
+        input_size_train = (1, 3, 224, 224)
         info = summary(model, input_size_train, verbose=0, col_names=("output_size", "num_params", "mult_adds"))
 
-        channels_list.append(channels)
+        channels_list.append(ratio_channels)
         macs2.append(info.total_mult_adds)
-        memories2.append(info.max_memory)
-        train_crop_size2.append(232)
-        print(channels, info.total_mult_adds, info.max_memory)
-    
 
-
+        if np.abs(info.total_mult_adds - target) < np.abs(best - target) :
+            best = info.total_mult_adds
+            best_channels = ratio_channels
+        memories2.append(0)
+        #memories2.append(info.max_memory)
+        train_crop_size2.append(224)
+        # print(channels, info.total_mult_adds, info.max_memory)
+    print(best, best_channels)
     # Save results in csv files
     header = ["Channels", "Channels", "Channels", "Channels", "Input Resolution", "Total Mults Adds", "Max Memory"]
     with open("results1.csv", "w+") as file :
@@ -97,7 +127,7 @@ if __name__ == "__main__" :
         csvwriter = csv.writer(file)
         csvwriter.writerow(header)
         for channels, mac, memory in zip(channels_list, macs2, memories2) :
-            csvwriter.writerow(list(channels) + [232, mac, memory])
+            csvwriter.writerow(list(channels) + [224, mac, memory])
 
     plt.figure(1, figsize=(10, 10))
     #plt.subplot(121)
@@ -113,7 +143,7 @@ if __name__ == "__main__" :
     plt.title("Resize Resolution on model ")
 
     plt.savefig("model.png")
-    tkplot.save("model.tex")
+    # tkplot.save("model.tex")
     plt.close()
 
 
@@ -131,5 +161,5 @@ if __name__ == "__main__" :
     plt.title("Resize Resolution on model ")
 
     plt.savefig("model3.png")
-    tkplot.save("model3.tex")
+    # tkplot.save("model3.tex")
     plt.close()
